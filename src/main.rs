6378e92152a80b9ac84b338a8d20a64cfe567734
Goran Vladika <goran.vladika@gmail.com>
2023-09-15 17:15:27 +0200

    Use counter and add check before burning

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,11 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 use crate::erc721::{Erc721, Erc721Params};
 use alloy_primitives::{Address, U256};
-use erc721::Erc721Error;
+use erc721::{Erc721Error, NotAuthorized};
 /// Import the Stylus SDK along with alloy primitive types for use in our program.
-use stylus_sdk::prelude::*;
+use stylus_sdk::{msg, prelude::*};
 
-/// import module
+/// import modules
 mod erc721;
 
 struct StylusNFTParams;
@@ -32,18 +32,30 @@ sol_storage! {
     struct StylusNFT {
         #[borrow] // Allows erc721 to access MyToken's storage and make calls
         Erc721<StylusNFTParams> erc721;
+        uint256 counter;
     }
 }
 
 #[external]
 #[inherit(Erc721<StylusNFTParams>)]
 impl StylusNFT {
-    pub fn mint(&mut self, to: Address, token_id: U256) -> Result<(), Erc721Error> {
+    pub fn mint(&mut self, to: Address) -> Result<(), Erc721Error> {
+        let token_id = self.counter.get();
         self.erc721._mint(to, token_id)?;
+
+        let new_value = token_id + U256::from(1);
+        self.counter.set(new_value);
         Ok(())
     }
 
     pub fn burn(&mut self, token_id: U256) -> Result<(), Erc721Error> {
+        let owner = self.erc721._owners.get(token_id);
+        if msg::sender() != owner {
+            return Err(Erc721Error::NotAuthorized(NotAuthorized {
+                caller: msg::sender(),
+            }));
+        };
+
         self.erc721._burn(token_id)?;
         Ok(())
     }
